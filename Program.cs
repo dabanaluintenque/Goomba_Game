7643e3dd4ffa2da7dcd90af260e1c83acbe80d52
Dabana Intenque <64395601+dabanaluintenque@users.noreply.github.com>
2022-03-17 14:30:44 -0400

    Goomba Game

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*
+Star star = new Star(10);
+
+ star.DrawSprite();
+
+ Ground ground = new Ground(50);
+ ground.DrawSprite();  */
+
+/*
+
+Star star = new Star(4);
+ Goomba goomba = new Goomba(4);
+
+ Ground ground = new Ground(120);
+
+ GoombaWalk gWalk = new GoombaWalk(star, goomba,ground);
+
+ gWalk.StartAnimation();
+
+ 
+
+ ParaGoomba pGoomba = new ParaGoomba(4);
+
+ pGoomba.DrawSprite(); */
+
+
+
+
+Star star = new Star(4);
+ Goomba goomba = new Goomba(4);
+
+// Ground ground = new Ground(4);
+
+ ParaGoomba pGoomba = new ParaGoomba(4);
+
+ Ground ground = new Ground(120);
+ GoombaWalk gWalk = new GoombaWalk(star, pGoomba,ground);
+ Console.Clear();
+ gWalk.StartAnimation();
+
+
+ 
+
+ 
+
